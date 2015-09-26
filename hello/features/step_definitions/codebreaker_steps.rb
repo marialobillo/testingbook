15575124aa6c88c58a94d2aaeda74046ddbebf72
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-09-26 15:15:43 +0200

    zombie

--- hello/features/step_definitions/codebreaker_steps.rb
+++ hello/features/step_definitions/codebreaker_steps.rb
@@ -1,3 +1,7 @@
 Given /^I am not yey playing$/ do
 
 end
+
+When /^I start a new game$/ do
+  Codebreaker::Game.new.start
+end
---
+++ rspec/lib/zombie_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'A zombie' do
+  it 'is named Ash'
+end
