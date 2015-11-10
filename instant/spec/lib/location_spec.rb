117924d8de6dee0c13f027284a5683d508198a97
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-10 17:07:02 +0100

    set lat and long

--- instant/spec/lib/location_spec.rb
+++ instant/spec/lib/location_spec.rb
@@ -1,5 +1,27 @@
 require "spec_helper"
 
-describe Location do
+class Location
+  def initialize(args = {}); end
+  def latitude
+    40.39258072
+  end
+  def longitude
+    -3.69140625
+  end
+end
 
+describe Location do
+  describe "#initialize" do
+    it "sets the latitude and longitud" do
+      loc = Location.new(:latitude => 40.39258072,
+                         :longitude => -3.69140625)
+      loc.latitude.should == 40.39258072
+      loc.longitude.should ==  -3.69140625
+    end
+    it "sets the latitude to 0 and longitude to 1" do
+      loc = Location.new(:latitude => 0, :longitude => 1)
+      loc.latitude.should == 0
+      loc.longitude.should == 1
+    end
+  end
 end
--- instant/spec/spec_helper.rb
+++ instant/spec/spec_helper.rb
@@ -93,6 +93,12 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.warnings = false
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
-
-
