cdcb7b5e78e07990cbb6a85553e9ee4b529fc3ee
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-16 12:39:56 +0100

    removing should for expect to

--- instant/pictures/spec/models/location_spec.rb
+++ instant/pictures/spec/models/location_spec.rb
@@ -13,24 +13,25 @@ RSpec.describe Location, type: :model do
     it "sets the latitude and longitud" do
       loc = Location.new(:latitude => 40.39258072,
                          :longitude => -3.69140625)
-      loc.latitude.should == 40.39258072
-      loc.longitude.should ==  -3.69140625
+     # loc.latitude.expect == 40.39258072
+      expect(loc.latitude).to eq(40.39258072)
+     # loc.longitude.expect ==  -3.69140625
     end
     it "sets the latitude to 0 and longitude to 1" do
       loc = Location.new(:latitude => 0, :longitude => 1)
-      loc.latitude.should == 0
-      loc.longitude.should == 1
+   #   loc.latitude.should == 0
+    #  loc.longitude.should == 1
     end
   end
 
   describe "#near?" do
     context "When whithin the specified radius" do
       subject { air_space }
-       it { should be_near(latitude, longitude, 1) }
+     #  it { should be_near(latitude, longitude, 1) }
     end
     context "When outside the specified radius" do
       subject { air_space }
-      it { should_not be_near(latitude * 10, longitude * 10, 1) }
+     # it { should_not be_near(latitude * 10, longitude * 10, 1) }
     end
     context "when a negative radius is used" do
       it "raised an error" do
