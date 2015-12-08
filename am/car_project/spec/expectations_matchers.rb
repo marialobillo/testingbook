4b7da90a1b103e5464eab8305167c9a1d569d18a
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-08 16:51:16 +0100

    object types

--- am/car_project/spec/expectations_matchers.rb
+++ am/car_project/spec/expectations_matchers.rb
@@ -68,11 +68,24 @@ describe 'Expectation Matchers' do
   describe 'other useful matchers' do
 
     it 'will match string with a regex' do
+      string = 'The order has been received.'
+      expect(string).to match(/order(.+)received/)
 
+      expect('123').to match(/\d{3}/)
+      expect(123).not_to match(/\d{3}/)
+
+      email = 'someone@somewhere.com'
+      expect(email).to match(/\A\w+@\w+\.\w{3}\Z/)
     end
 
     it 'will match object types' do
+      expect('test').to be_instance_of(String)
+      expect('test').to be_instance_of(String)
 
+      expect('test').to be_kind_of(String)
+      expect('test').to be_a_kind_of(String)
+      expect('test').to be_a(String)
+      expect([1,2,3]).to be_an(Array)
     end
 
     it 'will match objects with #respond_to' do
@@ -84,7 +97,7 @@ describe 'Expectation Matchers' do
     end
 
     it 'will match anything with #satisfy' do
-      
+
     end
   end
 
