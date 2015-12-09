9ff41add057d90fad19ea2a4ad3e442fffe6b7a6
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-09 17:42:59 +0100

    expect expect expect

--- am/car_project/spec/expectations_matchers.rb
+++ am/car_project/spec/expectations_matchers.rb
@@ -156,21 +156,48 @@ describe 'Expectation Matchers' do
   end
 
   describe 'observation matchers' do
-
+    # expect {}
     it 'will match when events change object attributes' do
+      array = []
+      expect { array << 1 }.to change(array, :empty?).from(true).to(false)
 
+      class WebsitesHits
+        attr_accessor :count
+        def initialize; @count = 0; end
+        def increment; @count += 1; end
+      end
+      hits = WebsitesHits.new
+      expect { hits.increment }.to change(hits, :count).from(0).to(1)
     end
 
     it 'will match when events change any values' do
+      x = 10
+      expect { x += 1 }.to change {x}.from(10).to(11)
+      expect { x += 1 }.to change {x}.by(1)
+      expect { x += 1 }.to change {x}.by_at_least(1)
+      expect { x += 1 }.to change {x}.by_at_most(1)
 
+      z = 11
+      expect { z += 1 }.to change { z % 3 }.from(2).to(0)
     end
 
     it 'will match when errors are raised' do
+      expect { raise StandarError }.to raise_error
+      expect { raise StandarError }.to raise_exception
 
+      expect { 1 / 0 }.to raise_error(ZeroDivisionError)
+      expect { 1 / 0 }.to raise_error.with_message("divided by 0")
+      expect { 1 / 0 }.to raise_error.with_message(/divided/)
+
+      expect { 1 / 1 }.not_to raise_error
     end
 
     it 'will match when output is generated' do
-      
+      expect { print('hello') }.to output.to_stdout
+      expect { print('hello') }.to output('hello').to_stdout
+      expect { print('hello') }.to output(/ll/).to_stdout
+
+      expect { warn('problem')}.to output(/problem/).to_stderr
     end
 
   end
