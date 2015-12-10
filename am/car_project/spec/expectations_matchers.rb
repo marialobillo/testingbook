81fb93034f43a79a02dbcb0c7037445e0700f774
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-10 14:45:01 +0100

    one more

--- am/car_project/spec/expectations_matchers.rb
+++ am/car_project/spec/expectations_matchers.rb
@@ -244,6 +244,10 @@ describe 'Expectation Matchers' do
       expect(fruits).to start_with(a_string_starting_with('a') ) &
         include( a_string_matching(/a.a.a/) ) &
         end_with( a_string_ending_with('y') )
+
+      array = [1,2,3,4]
+      expect(array).to start_with( a_value <= 2) |
+        end_with( a_value_within(1).of(5) )
     end
   end
 end
