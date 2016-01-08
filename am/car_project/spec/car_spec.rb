9d693c16aa870d6c93322c7610f7c8cff67bba24
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-01-08 13:01:39 +0100

    before hook

--- am/car_project/spec/car_spec.rb
+++ am/car_project/spec/car_spec.rb
@@ -4,27 +4,27 @@ describe 'Car' do
 
   describe 'atttributes' do
 
+    before(:example) do
+      @car = Car.new
+    end
+
     it "allows reading and writing for :make" do
-      car = Car.new
-      car.make = 'Test'
-      expect(car.make).to eq('Test')
+      @car.make = 'Test'
+      expect(@car.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
-      car = Car.new
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      @car.year = 9999
+      expect(@car.year).to eq(9999)
     end
 
     it "allows reading and writing for :color" do
-      car = Car.new
-      car.color = 'foo'
-      expect(car.color).to eq('foo')
+      @car.color = 'foo'
+      expect(@car.color).to eq('foo')
     end
 
     it "allows reading for :wheels" do
-      car = Car.new
-      expect(car.wheels).to eq(4)
+      expect(@car.wheels).to eq(4)
     end
 
     it "allows writing for :doors" do
